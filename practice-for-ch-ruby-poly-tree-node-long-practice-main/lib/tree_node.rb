11af91f3bec06ee43e0fc11a5cf52dd2f6519c54
“Spencer <Spencer@bookservo.com>
2023-09-08 15:19:47 -0400

    finish dfs method

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -31,20 +31,26 @@ class PolyTreeNode
     end
 
     def dfs(target)
-        return self if self.value == target
-        # return nil if self.children.empty?
+        return self if @value == target
 
-        (0..self.children.length-1).each do |i|
-            # if self.children[i].dfs(target).nil?
-            #     i = i+1
-            # end
-            self.children[i].dfs(target)
+        if !self.children.empty?
+            @children.each do |child|
+                value = child.dfs(target)
+                return value if value != nil
+            end
         end
-        
         nil
     end
 
+        # return nil if self.children.empty?
 
-
-
+        # (0..self.children.length-1).each do |i|
+        #     # if self.children[i].dfs(target).nil?
+        #     #     i = i+1
+        #     # end
+        #     self.children[i].dfs(target)
+        # end
+        
+        # nil
+    # end
 end
