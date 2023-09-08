8c8b886e52b61482000be654bdda9381397fc183
“Spencer <Spencer@bookservo.com>
2023-09-08 13:51:44 -0400

    work on parent= function

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -3,22 +3,26 @@ class PolyTreeNode
 
     def initialize(value)
         @value = value
-        @parent = nil
+        @parent = nil 
         @children = []
     end
 
     def parent=(parent_node)
-        if self.parent.nil?
-            @parent = parent_node
-            parent_node.children << self
-        else
-            self_parent = self.parent
-            self_parent.children.each_with_index do |node, i|
-                self.parent = nil if self == node
+        # if parent_node.nil?
+        #     self.parent = nil
+        # end
+        # if self.parent.nil?
+        #     @parent = parent_node
+        #     parent_node.children << self
+        # else
+        #     self.parent = parent_node
+        # end
+        # else
+        #     self_parent = self.parent
+        #     self_parent.children.each_with_index do |node, i|
+        #         self.parent = nil if self == node
                 
-            end
-
-        end
+        #     end
     end
 
 
