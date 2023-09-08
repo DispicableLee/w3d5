a8ea8a1dd8974172808e9a44dd0e7dd673061eaf
DispicableLee <lee.robert053@gmail.com>
2023-09-08 13:32:48 -0400

    cant figure out .children

--- practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
+++ practice-for-ch-ruby-poly-tree-node-long-practice-main/lib/tree_node.rb
@@ -1,3 +1,27 @@
 class PolyTreeNode
-  
+    attr_reader :value, :parent, :children
+
+    def initialize(value)
+        @value = value
+        @parent = nil
+        @children = []
+    end
+
+    def parent=(parent_node)
+        if self.parent.nil?
+            @parent = parent_node
+            parent_node.children << self
+        else
+            self_parent = self.parent
+            self_parent.children.each_with_index do |node, i|
+                self.parent = nil if self == node
+                
+            end
+
+        end
+    end
+
+
+
+
 end
